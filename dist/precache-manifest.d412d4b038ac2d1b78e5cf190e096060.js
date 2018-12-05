ec70142821e44ac3c9b1e43ca8ed9cc342b9e0dc
Sukhjit Singh <sukhjitsingh@Sukhjits-MBP.hitronhub.home>
2018-12-04 19:23:28 -0700

    main-project

---
+++ dist/precache-manifest.d412d4b038ac2d1b78e5cf190e096060.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "470d535a3857af878caa",
+    "url": "/static/css/main.f238eaeb.chunk.css"
+  },
+  {
+    "revision": "470d535a3857af878caa",
+    "url": "/static/js/main.470d535a.chunk.js"
+  },
+  {
+    "revision": "0f895355d5d378defbf0",
+    "url": "/static/js/1.0f895355.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69b60520a87aa4ea24822a9675f770bd",
+    "url": "/index.html"
+  }
+];
